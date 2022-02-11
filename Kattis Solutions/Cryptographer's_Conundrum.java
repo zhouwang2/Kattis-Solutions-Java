fd9a8ff668550766757b93a59c609589608009e0
Zhou Wang <zhouwang2@yahoo.com.au>
2022-02-11 16:33:25 +1100

    Update Cryptographer's_Conundrum.java

--- Kattis Solutions/Cryptographer's_Conundrum.java
+++ Kattis Solutions/Cryptographer's_Conundrum.java
@@ -1,22 +1,25 @@
 import java.util.Scanner;
 
-public class Conundrum{
-    public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        String word = reader.next();
-        reader.close();
+public class Kattis {
 
-        int count = 0;
-        for(int i = 0; i < word.length(); i++){
-            if(word.charAt(i) == 'P' && i % 3 == 0)
-                continue;
-            if(word.charAt(i) == 'E' && i % 3 == 1)
-                continue;
-            if(word.charAt(i) == 'R' && i % 3 == 2)
-                continue;
-            count++;
-        }
-
-        System.out.println(count);
-    }
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.next();
+		
+		int total = 0;
+		
+		for (int i = 0; i < s.length(); i++){
+	        if (i%3 == 0 && s.charAt(i) != 'P')
+	            total++;
+	        if (i%3 == 1 && s.charAt(i) != 'E')
+	            total++;
+	        if (i%3 == 2 && s.charAt(i) != 'R')
+	           total++;
+	    }
+		
+		System.out.println(total);
+		
+	}
 }
